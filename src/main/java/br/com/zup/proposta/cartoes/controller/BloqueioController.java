f6ce0ad489accc43a63321cc2ce958e4128d4624
jonas-souza-zup <jonas.souza@zup.com.br>
2021-03-16 15:38:12 -0300

    Refatoração do bloqueio de cartão

--- src/main/java/br/com/zup/proposta/cartoes/controller/BloqueioController.java
+++ src/main/java/br/com/zup/proposta/cartoes/controller/BloqueioController.java
@@ -1,34 +1,34 @@
 package br.com.zup.proposta.cartoes.controller;
 
-import br.com.zup.proposta.cartoes.controller.request.NovoBloqueioRequest;
 import br.com.zup.proposta.cartoes.httpclient.CartaoClient;
-import br.com.zup.proposta.cartoes.repository.BloqueioRepository;
+import br.com.zup.proposta.cartoes.model.Bloqueio;
 import br.com.zup.proposta.cartoes.repository.CartaoRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
-import javax.validation.Valid;
 
 @RestController
 public class BloqueioController {
     private final CartaoRepository cartaoRepository;
-    private final BloqueioRepository bloqueioRepository;
     private final CartaoClient cartaoClient;
 
-    public BloqueioController(CartaoRepository cartaoRepository, BloqueioRepository bloqueioRepository, CartaoClient cartaoClient) {
+    public BloqueioController(CartaoRepository cartaoRepository, CartaoClient cartaoClient) {
         this.cartaoRepository = cartaoRepository;
-        this.bloqueioRepository = bloqueioRepository;
         this.cartaoClient = cartaoClient;
     }
 
     @PostMapping("/api/cartoes/{id}/bloquear")
     @Transactional
-    public ResponseEntity<?> bloquearCartao(@PathVariable Long id, @RequestBody @Valid NovoBloqueioRequest request) {
+    public ResponseEntity<?> bloquearCartao(@PathVariable Long id, HttpServletRequest request) {
         var cartao = cartaoRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cartão não encontrado"));
-        cartao.bloquear(request.toModel(cartao), cartaoClient);
+        cartao.bloquear(new Bloqueio(cartao, request.getRemoteAddr(), request.getHeader("User-Agent")), cartaoClient);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
+
 }
